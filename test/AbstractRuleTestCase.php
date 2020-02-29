f679ae2f3d6caf1b291d98257144faef46bd8664
Marc MOREAU <moreau.marc.web@gmail.com>
2020-02-29 01:55:38 +0100

    code style fix

--- test/AbstractRuleTestCase.php
+++ test/AbstractRuleTestCase.php
@@ -1,5 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 
+/**
+ * @author Marc MOREAU <moreau.marc.web@gmail.com>
+ * @license https://github.com/request-for-comments/5234-PHP/blob/master/LICENSE.md CC-BY-SA-4.0
+ * @link https://github.com/request-for-comments/5234-PHP/blob/master/README.md
+ */
 
 namespace RFC5234\Test;
 
